1fa22d72d5674236ca441a70a820eab267e59984
kaplanebru <kaplanebru@users.noreply.github.com>
2023-08-31 23:04:46 +0300

    tower height ui

---
+++ Scripts/Towers/TowerUIHandler.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class TowerUIHandler : MonoBehaviour
+{
+   public TextMeshPro heightText;
+
+   private void OnEnable() //TODO: Tower scriptinden yönet
+   {
+      Eventbus.UIEvents.OnTowerHeightChange += ChangeHeightUI;
+   }
+
+   void ChangeHeightUI(float height, Tower tower) //DoTween
+   {
+      if(tower.gameObject != gameObject) return;
+      
+      int heightInt = Mathf.RoundToInt(height);
+      heightText.text = heightInt.ToString();
+   }
+
+   private void OnDisable()
+   {
+      Eventbus.UIEvents.OnTowerHeightChange -= ChangeHeightUI;
+
+   }
+}
