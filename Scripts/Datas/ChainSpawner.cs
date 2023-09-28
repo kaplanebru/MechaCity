61c7799bcfc4e17da41ece6fdddb7aa4d4e480b2
kaplanebru <kaplanebru@users.noreply.github.com>
2023-09-28 22:47:59 +0300

    arc

--- Scripts/Datas/ChainSpawner.cs
+++ Scripts/Datas/ChainSpawner.cs
@@ -9,47 +9,48 @@ public enum LineType
     Straight,
     Curved
 }
+
 public class ChainSpawner : MonoBehaviour
 {
     public LineRenderer lr;
-    
+
     public int curveAmount = 15;
     public int straightAmount = 15;
-    
+
     public Vector3[] curveEdges;
     public Vector3 lineEdge;
-    
-    [ReadOnly]public List<Vector3> chainCurvePoints = new();
-    [ReadOnly]public List<Vector3> chainStraightPoints = new();
+
+    [ReadOnly] public List<Vector3> chainCurvePoints = new();
+    //[ReadOnly] public List<Vector3> chainStraightPoints = new();
 
     private void Start()
     {
-        GeneratePoints(curveAmount, LineType.Curved);
-        GeneratePoints(curveAmount, LineType.Straight);
-        lr.positionCount = straightAmount; //+curveAmount;
-        DrawStraightLine();
-        //DrawChain();
+        chainCurvePoints.Clear();
+        lr.positionCount = 0;
+        GeneratePoints(curveAmount+straightAmount);
         
+        lr.positionCount = curveAmount * 4;
+        lr.SetPositions(chainCurvePoints.ToArray());
+        //DrawStraightLine();
+        DrawChain();
     }
 
     void DrawChain()
     {
+        // for (int i = straightAmount; i < lr.positionCount; i++)
+        // {
+        //     lr.SetPosition(i, chainCurvePoints[i]);
+        // }
         
-        
-        
-        for (int i = straightAmount; i < lr.positionCount; i++)
-        {
-            lr.SetPosition(i, chainCurvePoints[i]);
-        }
     }
 
-    void DrawStraightLine()
-    {
-        for (int i = 0; i < lr.positionCount; i++)  //straightCount
-        {
-            lr.SetPosition(i, chainStraightPoints[i]);
-        }
-    }
+    // void DrawStraightLine()
+    // {
+    //     for (int i = 0; i < lr.positionCount; i++) //straightCount
+    //     {
+    //         lr.SetPosition(i, chainStraightPoints[i]);
+    //     }
+    // }
 
     Vector3 CurvePoint(float t)
     {
@@ -57,37 +58,30 @@ public class ChainSpawner : MonoBehaviour
         Vector3 BC = Vector3.Lerp(curveEdges[1], curveEdges[2], t);
         return Vector3.Lerp(AB, BC, t);
     }
-    
+
     Vector3 StraightPoint(float t)
     {
         return Vector3.Lerp(lineEdge, curveEdges[0], t);
     }
-    
-    void GeneratePoints(float _amount, LineType lineType)
+
+    void GeneratePoints(float _amount)
     {
         float ratio = 1f / _amount;
         float t = 0;
-        
-        switch (lineType)
+
+
+        while (t < 1)
         {
-            case LineType.Straight:
-                while (t < 1) 
-                {
-                    t = Mathf.MoveTowards(t, 1, ratio);
-                    chainStraightPoints.Add(StraightPoint(t));
-                }
-                break;
-            case LineType.Curved:
-                while (t < 1) 
-                {
-                    t = Mathf.MoveTowards(t, 1, ratio);
-                    chainCurvePoints.Add(CurvePoint(t));
-                }
-                break;
+            t = Mathf.MoveTowards(t, 1, ratio);
+            chainCurvePoints.Add(StraightPoint(t));
         }
-        
+
+        t = 0;
+        while (t < 1)
+        {
+            t = Mathf.MoveTowards(t, 1, ratio);
+            chainCurvePoints.Add(CurvePoint(t));
+        }
     }
-
-    
-
 }
+
