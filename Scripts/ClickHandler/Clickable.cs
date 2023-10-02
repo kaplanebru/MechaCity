1e077b02828ecb910b78dc30140b23b2f5581265
kaplanebru <kaplanebru@users.noreply.github.com>
2023-10-02 20:45:30 +0300

    ++

--- Scripts/ClickHandler/Clickable.cs
+++ Scripts/ClickHandler/Clickable.cs
@@ -6,7 +6,7 @@ namespace Clicks
 {
     public class Clickable : BaseClickable<int> //uniqID
     {
-        public int id; //for multiplayer
+        public int id;
         public TeamType teamType;
    
 
