2516032c7d9a66b168b1a21ca2db5113b88e4d64
kaplanebru <kaplanebru@users.noreply.github.com>
2023-10-23 20:21:18 +0300

    data removed from cog holder

--- Scripts/Chain/Active/Cog/CogHolder.cs
+++ Scripts/Chain/Active/Cog/CogHolder.cs
@@ -10,35 +10,24 @@ namespace Chain
     [Serializable]
     public class CogHolder
     {
-        public CogData Data;
+        CogData Data;
         //public Transform CogObject;
-       
+
         public Cogwheel cog;
 
         public void SetCogData()
         {
-            //cog = (Cogwheel)EditorGUILayout.ObjectField("Cog Object", cog, typeof(Cogwheel), true);
-            // cog = EditorGUILayout.ObjectField("Cog", cog, typeof(Cogwheel), true) as Cogwheel;
-            // Data = (CogData) EditorGUILayout.ObjectField("Cog Data", Data, typeof(CogData), false);
-            //
-            // if (Data == null || cog == null)
-            // {
-            //     if(Data == null)
-            //         Debug.Log("cog data null");
-            //     if(cog == null)
-            //         Debug.Log("cog null");
-            //     return;
-            // }
-            
+            Data = cog.Data;
             Data.Radius = EditorGUILayout.FloatField("Radius", Data.Radius);
             Data.toothScale = EditorGUILayout.Vector3Field("Tooth Scale", Data.toothScale);
             Data.circularThickness = EditorGUILayout.FloatField("Thickness", Data.circularThickness);
-            //Data.cogObject = (Transform) EditorGUILayout.ObjectField("Cog Object", Data.cogObject, typeof(Transform), true);
-            
-
             Data.cogObject = cog.cogObject;
-            Debug.Log( Data.cogObject.name);
-           // Undo.RecordObject(cog, "Cog");
+            
+            //cog = (Cogwheel)EditorGUILayout.ObjectField("Cog Object", cog, typeof(Cogwheel), true);
+            // cog = EditorGUILayout.ObjectField("Cog", cog, typeof(Cogwheel), true) as Cogwheel;
+            // Data = (CogData) EditorGUILayout.ObjectField("Cog Data", Data, typeof(CogData), false);
+            //Data.cogObject = (Transform) EditorGUILayout.ObjectField("Cog Object", Data.cogObject, typeof(Transform), true);
+            // Undo.RecordObject(cog, "Cog");
 
         }
         
