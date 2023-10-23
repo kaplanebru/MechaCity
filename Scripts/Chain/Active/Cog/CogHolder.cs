76da8b66414170dbb82fb35c9772d8466e5358ee
kaplanebru <kaplanebru@users.noreply.github.com>
2023-10-23 20:17:51 +0300

    fill problem fixed

--- Scripts/Chain/Active/Cog/CogHolder.cs
+++ Scripts/Chain/Active/Cog/CogHolder.cs
@@ -10,26 +10,25 @@ namespace Chain
     [Serializable]
     public class CogHolder
     {
-        [SerializeField]
         public CogData Data;
         //public Transform CogObject;
-        [SerializeField]
+       
         public Cogwheel cog;
 
         public void SetCogData()
         {
             //cog = (Cogwheel)EditorGUILayout.ObjectField("Cog Object", cog, typeof(Cogwheel), true);
-            cog = EditorGUILayout.ObjectField("Cog", cog, typeof(Cogwheel), true) as Cogwheel;
-            Data = (CogData) EditorGUILayout.ObjectField("Cog Data", Data, typeof(CogData), false);
-            
-            if (Data == null || cog == null)
-            {
-                if(Data == null)
-                    Debug.Log("cog data null");
-                if(cog == null)
-                    Debug.Log("cog null");
-                return;
-            }
+            // cog = EditorGUILayout.ObjectField("Cog", cog, typeof(Cogwheel), true) as Cogwheel;
+            // Data = (CogData) EditorGUILayout.ObjectField("Cog Data", Data, typeof(CogData), false);
+            //
+            // if (Data == null || cog == null)
+            // {
+            //     if(Data == null)
+            //         Debug.Log("cog data null");
+            //     if(cog == null)
+            //         Debug.Log("cog null");
+            //     return;
+            // }
             
             Data.Radius = EditorGUILayout.FloatField("Radius", Data.Radius);
             Data.toothScale = EditorGUILayout.Vector3Field("Tooth Scale", Data.toothScale);
@@ -37,9 +36,9 @@ namespace Chain
             //Data.cogObject = (Transform) EditorGUILayout.ObjectField("Cog Object", Data.cogObject, typeof(Transform), true);
             
 
-            //Data.cogObject = cog.cogObject;
-            Debug.Log("set cog data");
-            Undo.RecordObject(cog, "Cog");
+            Data.cogObject = cog.cogObject;
+            Debug.Log( Data.cogObject.name);
+           // Undo.RecordObject(cog, "Cog");
 
         }
         
