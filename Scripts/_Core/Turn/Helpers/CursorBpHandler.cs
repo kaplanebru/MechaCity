944852ff02e1b087839a6346465a526d83b2c799
kaplanebru <kaplanebru@users.noreply.github.com>
2024-03-04 20:48:56 +0300

    ++

--- Scripts/_Core/Turn/Helpers/CursorBpHandler.cs
+++ Scripts/_Core/Turn/Helpers/CursorBpHandler.cs
@@ -3,23 +3,34 @@ using System.Collections;
 using System.Collections.Generic;
 using DataModels;
 using UnityEngine;
+using DG.Tweening;
 
 public class CursorBpHandler 
 {
    private SpriteRenderer _spriteRenderer;
    private BlueprintData currentBpData;
+   private Transform _spriteTransform;
+   private Vector3 startSize;
 
    public CursorBpHandler(SpriteRenderer spriteRenderer)
    {
       _spriteRenderer = spriteRenderer;
+      
+      _spriteTransform = _spriteRenderer.GetComponent<Transform>();
+      startSize = _spriteTransform.localScale;
    }
   
 
    public void SetBlueprintImage(BlueprintData data)
    {
       currentBpData = data;
+      
+      _spriteTransform.localScale = Vector3.zero;
       _spriteRenderer.sprite = currentBpData.Sprite;
+      _spriteTransform.DOScale(startSize, .4f);
    }
+   
+  
 
    public void Reset()
    {
