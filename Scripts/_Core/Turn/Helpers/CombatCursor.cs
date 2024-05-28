2ded0ab507ec8046c7bc392a1d6bedebea66a7aa
kaplanebru <kaplanebru@users.noreply.github.com>
2024-05-28 23:12:56 +0300

    combat cursor revised

--- Scripts/_Core/Turn/Helpers/CombatCursor.cs
+++ Scripts/_Core/Turn/Helpers/CombatCursor.cs
@@ -1,19 +1,18 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using DataModels;
 using DG.Tweening;
 using Enums;
 using Network;
+using Teams;
 using Towers;
 using UnityEngine;
 
 public class CombatCursor : MonoBehaviour
 {
-    public float _currentAngle;
-    public float intervalAngle;
-    public GameObject line;
-    public GameObject cursor;
+    
     public BPDataHolder bpDataHolder;
     public BpInstallEffect installEffect;
 
@@ -24,13 +23,12 @@ public class CombatCursor : MonoBehaviour
     
     private void OnEnable()
     {
+        Eventbus.TeamEvents.OnTeamsSet += GetTransforms;
         Eventbus.CombatEvents.OnFire += ShiftTarget;
 
-        Eventbus.CombatEvents.OnCombatStarted += EnableLine;
-        Eventbus.CombatEvents.OnCombatEnding += DisableLine;
-
-        Eventbus.CombatEvents.OnCombatStarted += EnableCursor;
-        Eventbus.CombatEvents.OnCombatEnding += DisableCursor;
+        Eventbus.CombatEvents.OnCombatStarted += StartCursor;
+        Eventbus.CombatEvents.OnCombatEnding += EndCursor;
+        
 
         BpEventbus.UIEvents.OnBpInstallBegin += SetupAndInstall;
         BpEventbus.UIEvents.OnBpReset += ResetBpImage;
@@ -41,37 +39,56 @@ public class CombatCursor : MonoBehaviour
         installEffect.Initialize();
     }
 
+    private void GetTransforms(Team[] obj)
+    {
+        
+        for (int i = 0; i < AllTowers.TowersCount; i++)
+        {
+            transforms.Add(AllTowers.GetTower(i).transform);
+        }
+        Setup(); 
+    }
+
     void SetReferences()
     {
         spriteRenderer = GetComponentInChildren<SpriteRenderer>();
         cursorSpriteHandler = new CursorSpriteHandler(spriteRenderer);
     }
+    
 
-    void Start()
+    public List<Transform> transforms;
+    public List<Vector3> directions;
+    private Vector3 center;
+    public float directionOffset = 1;
+
+
+  
+    void SetDirections()
     {
-        Setup(); //initializera al
-        DisableLine();
+        foreach (var towerTransform in transforms)
+        {
+            var dir = (towerTransform.position - center).normalized;
+            directions.Add(center + new Vector3(dir.x * directionOffset, 0, dir.z * directionOffset));
+        }
     }
-
     void Setup()
     {
         SetReferences();
-
-        _currentAngle = transform.eulerAngles.x;
-        intervalAngle = 360f / AllTowers.TowersCount;
+        center = transform.position;
+        SetDirections();
     }
 
     void ReverseAngle()
     {
-        intervalAngle *= -1;
+        directions.Reverse();
     }
 
+    private int index = 0;
     void ShiftTarget(float duration)
     {
-        DisableLine();
-
-        _currentAngle = (_currentAngle + intervalAngle) % 360;
-        transform.DORotate(new Vector3(0, _currentAngle, 0), duration).OnComplete(EnableLine);
+        index++;
+        index %= directions.Count;
+        transform.DOMove(index == 0 ? center : directions[index], duration); //.OnComplete(EnableLine);
     }
 
     void SetupAndInstall(BpType type)
@@ -88,36 +105,30 @@ public class CombatCursor : MonoBehaviour
         cursorSpriteHandler.ResetBpImage();
     }
 
-    void EnableLine()
+    void StartCursor()
     {
-        line.SetActive(true);
+        //EnableLine();
+        transform.DOMove(directions[0], .3f); //todo
     }
 
-    void DisableLine()
+    void EndCursor()
     {
-        line.SetActive(false);
     }
 
-    void EnableCursor()
-    {
-        cursor.SetActive(true);
-    }
+    
+   
 
-    void DisableCursor()
-    {
-        cursor.SetActive(false);
-    }
+  
 
 
     private void OnDisable()
     {
+        Eventbus.TeamEvents.OnTeamsSet -= GetTransforms;
         Eventbus.CombatEvents.OnFire -= ShiftTarget;
 
-        Eventbus.CombatEvents.OnCombatStarted -= EnableLine;
-        Eventbus.CombatEvents.OnCombatEnding -= DisableLine;
-
-        Eventbus.CombatEvents.OnCombatStarted -= EnableCursor;
-        Eventbus.CombatEvents.OnCombatEnding -= DisableCursor;
+        Eventbus.CombatEvents.OnCombatStarted -= StartCursor;
+        Eventbus.CombatEvents.OnCombatEnding -= EndCursor;
+        
         
         BpEventbus.UIEvents.OnBpInstallBegin -= SetupAndInstall;
         BpEventbus.UIEvents.OnBpReset -= ResetBpImage;
