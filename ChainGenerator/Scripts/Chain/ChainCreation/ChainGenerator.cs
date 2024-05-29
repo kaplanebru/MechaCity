57a113ab93dbca60164f3ae0846a1fdc3eccadda
kaplanebru <kaplanebru@users.noreply.github.com>
2024-05-29 15:33:28 +0300

    ++

--- ChainGenerator/Scripts/Chain/ChainCreation/ChainGenerator.cs
+++ ChainGenerator/Scripts/Chain/ChainCreation/ChainGenerator.cs
@@ -35,18 +35,9 @@ namespace Chain
                 Debug.LogWarning("Please Add Chain Data");
                 return;
             }
-
-            if (ChainData.LinkInterval <= 0)
-            {
-                print("3");
-                return;
-            }
+            if(ChainData.LinkInterval <= 0) return;
                 cogAmount = chainRelatedCogs.Length;
-                if (ChainData == null)
-                {
-                    print("2");
-                    return;
-                }
+            if(ChainData == null) return;
             if (ChainData.LinksPoolPrefab == null)
             {
                 Debug.LogWarning("Add Link Pool");
@@ -57,7 +48,6 @@ namespace Chain
             {
                 if(!PoolNull())
                     ResetLinks();
-                print("1");
                 return;
             }
 
