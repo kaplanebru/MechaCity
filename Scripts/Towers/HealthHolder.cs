8baeb14f5967b43ff25e28bbfd46fce900a4ecf5
kaplanebru <kaplanebru@users.noreply.github.com>
2024-05-30 12:29:10 +0300

    healt motion

---
+++ Scripts/Towers/HealthHolder.cs
@@ -0,0 +1,165 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using GameUI;
+using UnityEngine;
+
+public class HealthHolder : MonoBehaviour
+{
+    public HealthIcon iconPrefab;
+    public List<HealthIcon> icons;
+    private int _currentHealth;
+    [SerializeField] public float gap = 0.3f;
+    public int maxHealth = 3;
+    [SerializeField] private Transform parent;
+
+
+    private void OnEnable()
+    {
+        CreateIcons();
+        UIEventbus.OnHealthChange += AdjustIcons;
+    }
+
+    private void OnDisable()
+    {
+        UIEventbus.OnHealthChange -= AdjustIcons;
+    }
+    
+    void Activate(HealthIcon icon)
+    {
+        icon.gameObject.SetActive(true);
+        activeIcons.Add(icon);
+    }
+
+    public float range = 5;
+    float GetRandomAngle()
+    {
+        float random;
+        do
+        {
+            random = UnityEngine.Random.Range(-range, range);
+        } while (range == 0);
+
+        return random;
+    }
+
+    Vector2 GetRandomEulers() => new Vector2(GetRandomAngle(), GetRandomAngle());
+    
+
+    private void AdjustIcons(int health, GameObject towerGameObject)
+    {
+        if (towerGameObject != parent.gameObject) return;
+        
+        _currentHealth = health;
+        OrderIcons();
+    }
+    
+    public void CreateIcons() //int maxHealth
+    {
+        for (int i = 0; i < maxHealth; i++)
+        {
+            icons.Add(Instantiate(iconPrefab, transform));
+        }
+    }
+
+    private Vector2 _eulers;
+    
+    public void OrderIcons()
+    {
+        _eulers = GetRandomEulers();
+        DisableAll();
+        ResetAll();
+        if(_currentHealth == 0) return;
+
+        if (_currentHealth % 2 == 1)
+        {
+            OddOrder();
+        }
+        else
+        {
+            PairOrder();
+        }
+        
+        activeIcons.ForEach(i=>i.SetRotation(_eulers));
+    }
+
+    private List<HealthIcon> activeIcons = new();
+    void OddOrder()
+    {
+        int counter = 0;
+        Activate(icons[0]);
+
+        if(_currentHealth <= 1) return;
+        
+        for (int i = 1; i < _currentHealth; i++)
+        {
+            var icon = icons[i];
+            Activate(icon);
+            
+            if (i % 2 == 1)
+            {
+                counter++;
+                icon.transform.localPosition += new Vector3(counter * gap, 0, 0);
+            }
+            else
+            {
+                icon.transform.localPosition -= new Vector3(counter * gap, 0, 0);
+            }
+        }
+    }
+
+    
+    void PairOrder()
+    {
+        int counter = 0;
+        
+        icons[0].transform.localPosition += Vector3.right * gap / 2;
+        icons[1].transform.localPosition -= Vector3.right * gap / 2;
+        
+        Activate(icons[0]);
+        Activate(icons[1]);
+
+        if(_currentHealth <= 2) return;
+        
+        for (int i = 2; i < _currentHealth; i++)
+        {
+            var icon = icons[i];
+            Activate(icon);
+
+            if (i % 2 == 0)
+            {
+                counter++;
+                icon.transform.localPosition += new Vector3((counter * gap) + gap/2, 0, 0);
+            }
+            else
+            {
+                icon.transform.localPosition -= new Vector3((counter * gap) + gap/2, 0, 0);
+            }
+        }
+        
+    }
+
+    // void MoveIcons()
+    // {
+    //     foreach (var icon in activeIcons)
+    //     {
+    //         
+    //     }
+    // }
+
+    void DisableAll()
+    {
+        icons.ForEach(i => i.gameObject.SetActive(false));
+        activeIcons.Clear();
+    }
+
+    void ResetAll()
+    {
+        foreach (var icon in icons)
+        {
+            Vector3 pos = icons[0].transform.localPosition;
+            pos.x = 0;
+            icon.transform.localPosition = pos;
+        }
+    }
+}
--- Scripts/Towers/HealthIcon.cs
+++ Scripts/Towers/HealthIcon.cs
@@ -1,145 +1,46 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using GameUI;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class HealthIcon : MonoBehaviour
 {
-    public Transform iconPrefab;
-    public List<Transform> icons;
-    private int _currentHealth;
-    [SerializeField] public float gap = 0.3f;
-    public int maxHealth = 3;
-    [SerializeField] private Transform parent;
+    public float range = 5;
+    public float speed = .3f;
 
+    private Vector2 _eulers;
 
     private void OnEnable()
     {
-        CreateIcons();
-        UIEventbus.OnHealthChange += AdjustIcons;
+        // _eulers[0] = GetRandomAngle();
+        // _eulers[1] = GetRandomAngle();
     }
 
-    private void OnDisable()
+    private void Update()
     {
-        UIEventbus.OnHealthChange -= AdjustIcons;
-    }
-    
-    void Activate(Transform icon)
-    {
-        icon.gameObject.SetActive(true);
-        activeIcons.Add(icon);
-    }
-
-    private void AdjustIcons(int health, GameObject towerGameObject)
-    {
-        if (towerGameObject != parent.gameObject) return;
-        
-        _currentHealth = health;
-        OrderIcons();
-    }
-    
-    public void CreateIcons() //int maxHealth
-    {
-        for (int i = 0; i < maxHealth; i++)
-        {
-            icons.Add(Instantiate(iconPrefab, transform));
-        }
-    }
-
-    public void OrderIcons()
-    {
-        DisableAll();
-        ResetAll();
-        if(_currentHealth == 0) return;
-
-        if (_currentHealth % 2 == 1)
-        {
-            OddOrder();
-        }
-        else
-        {
-            PairOrder();
-        }
-    }
-
-    private List<Transform> activeIcons = new();
-    void OddOrder()
-    {
-        int counter = 0;
-        Activate(icons[0]);
-
-        if(_currentHealth <= 1) return;
-        
-        for (int i = 1; i < _currentHealth; i++)
-        {
-            var icon = icons[i];
-            Activate(icon);
-            
-            if (i % 2 == 1)
-            {
-                counter++;
-                icon.transform.localPosition += new Vector3(counter * gap, 0, 0);
-            }
-            else
-            {
-                icon.transform.localPosition -= new Vector3(counter * gap, 0, 0);
-            }
-        }
+        Move();
     }
 
     
-    void PairOrder()
+    void Move()
     {
-        int counter = 0;
-        
-        icons[0].transform.localPosition += Vector3.right * gap / 2;
-        icons[1].transform.localPosition -= Vector3.right * gap / 2;
-        
-        Activate(icons[0]);
-        Activate(icons[1]);
-
-        if(_currentHealth <= 2) return;
-        
-        for (int i = 2; i < _currentHealth; i++)
-        {
-            var icon = icons[i];
-            Activate(icon);
-
-            if (i % 2 == 0)
-            {
-                counter++;
-                icon.transform.localPosition += new Vector3((counter * gap) + gap/2, 0, 0);
-            }
-            else
-            {
-                icon.transform.localPosition -= new Vector3((counter * gap) + gap/2, 0, 0);
-            }
-        }
-        
+        transform.Rotate(new Vector3(_eulers[0], 0, 0) * speed * Time.deltaTime);
     }
 
-    // void MoveIcons()
+    public void SetRotation(Vector2 eulers)
+    {
+        _eulers = eulers;
+    }
+    
+    // float GetRandomAngle()
     // {
-    //     foreach (var icon in activeIcons)
+    //     float random;
+    //     do
     //     {
-    //         
-    //     }
+    //         random = Random.Range(-range, range);
+    //     } while (range == 0);
+    //
+    //     return random;
     // }
-
-    void DisableAll()
-    {
-        icons.ForEach(i => i.gameObject.SetActive(false));
-        activeIcons.Clear();
-    }
-
-    void ResetAll()
-    {
-        foreach (var icon in icons)
-        {
-            Vector3 pos = icons[0].transform.localPosition;
-            pos.x = 0;
-            icon.transform.localPosition = pos;
-        }
-    }
 }
