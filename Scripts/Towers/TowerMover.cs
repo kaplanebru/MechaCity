77f871d8f3a03391cfe53a2ffcc5caccd0dc8dd8
kaplanebru <kaplanebru@users.noreply.github.com>
2024-06-11 13:19:41 +0300

    shake separated

--- Scripts/Towers/TowerMover.cs
+++ Scripts/Towers/TowerMover.cs
@@ -22,71 +22,55 @@ namespace Towers
         public TowerMoverData Data;
         public CombatTimingData timingData;
         private Rotater rotater;
+        private ShakeEffect shaker;
 
-        [Header("Shake")] float shakeMagnitude = 0.03f;
-        
+        [Header("Shake")] public float shakeMagnitude = 0.03f;
+
         public void Initialize()
         {
             rotater = new Rotater(Data.Middle.transform);
+            shaker = new ShakeEffect(new ShakeData(
+                Data.Middle.transform,
+                timingData.shakeDuration,
+                shakeMagnitude));
         }
 
-       
         public void ChangeHeight(float newHeight)
         {
             newHeight *= Data.HeightOffset;
             Data.Middle.transform.DOScaleY(newHeight, 1).OnComplete(() =>
             {
-                UIEventbus.OnTowerHeightChange?.Invoke(newHeight/Data.HeightOffset, gameObject);
+                UIEventbus.OnTowerHeightChange?.Invoke(newHeight / Data.HeightOffset, gameObject);
             });
 
             Data.Top.transform.DOLocalMoveY(newHeight + Data.TopOffset, 1);
         }
-        
+
         public void SetHeight(float newHeight)
         {
             newHeight *= Data.HeightOffset;
-            
+
             var scale = Data.Middle.transform.localScale;
             var pos = Data.Top.transform.localPosition;
-            
+
             scale.y = newHeight;
             pos.y = newHeight;
-            
+
             Data.Middle.transform.localScale = scale;
             Data.Top.transform.localPosition = pos;
-            
-            UIEventbus.OnTowerHeightChange?.Invoke(newHeight/Data.HeightOffset, gameObject);
+
+            UIEventbus.OnTowerHeightChange?.Invoke(newHeight / Data.HeightOffset, gameObject);
         }
 
         public void Shake()
         {
-            StartCoroutine(ShakeCoroutine(Data.Middle.transform));
+            StartCoroutine(shaker.ShakeCoroutine());
         }
 
-        private IEnumerator ShakeCoroutine(Transform middleTransform)
-        {
-            Vector3 originalPosition = middleTransform.localPosition;
-            float elapsed = 0.0f;
 
-            while (elapsed < timingData.shakeDuration)
-            {
-                float x = originalPosition.x + Random.Range(-1f, 1f) * shakeMagnitude;
-                float y = originalPosition.y + Random.Range(-1f, 1f) * shakeMagnitude;
-                float z = originalPosition.z + Random.Range(-1f, 1f) * shakeMagnitude;
-
-                middleTransform.localPosition = new Vector3(x, y, z);
-
-                elapsed += Time.deltaTime;
-
-                yield return null;
-            }
-
-            middleTransform.localPosition = originalPosition;
-        }
-        
         public void RotateMiddle()
         {
-           rotater.Rotate(360);
+            rotater.Rotate(360);
         }
     }
 }
