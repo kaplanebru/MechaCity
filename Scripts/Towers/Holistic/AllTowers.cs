82c4adee5b34f459166d99288ba279215f322c30
kaplanebru <kaplanebru@users.noreply.github.com>
2024-10-10 17:55:40 +0300

    ++

--- Scripts/Towers/Holistic/AllTowers.cs
+++ Scripts/Towers/Holistic/AllTowers.cs
@@ -9,13 +9,12 @@ namespace Towers
     public class AllTowers : MonoBehaviour
     {
         public static int TowersCount;
-
-        private static List<Tower> _towers = new();
-        private static List<TowerData> _towerDatas = new();
-        public static IEnumerable<Tower> Towers => _towers;
-        public static IEnumerable<TowerData> TowerDatas => _towerDatas;
-        public static Tower GetTower(int id) => _towers[id];
-        public static TowerData GetData(int id) => _towerDatas[id]; //todo? firstordefault? Ya da id'ye göre order ettir kesinliği için
+        public static List<Tower> Towers { get; private set; } = new();
+        public static List<TowerData> TowerDatas { get; private set; } = new();
+     
+        
+        public static Tower GetTower(int id) => Towers[id];
+        public static TowerData GetData(int id) => TowerDatas[id]; //todo? firstordefault? Ya da id'ye göre order ettir kesinliği için
 
         [SerializeField] Transform levelPrefab;
         Transform _level;
@@ -33,7 +32,7 @@ namespace Towers
 
         public static TowerData[] GetTowerGroup(IEnumerable<int> ids)
         {
-            return _towerDatas.Where(t => ids.Contains(t.UniqID)).ToArray();
+            return TowerDatas.Where(t => ids.Contains(t.UniqID)).ToArray();
         }
 
         private void ResetLinkedTowers(List<int> towerIds)
@@ -61,7 +60,7 @@ namespace Towers
             ReceiveTowers();
             ReceiveTowerData();
 
-            LinkingTowers(_towerDatas);
+            //LinkingTowers(_towerDatas);
             SettingNeighbours();
 
             GeneralEventbus.InitializerEvents.OnTowersCreated?.Invoke();
@@ -74,67 +73,67 @@ namespace Towers
 
         void ReceiveTowers()
         {
-            _towers = _level.GetComponentsInChildren<Tower>().ToList();
-            TowersCount = _towers.Count;
+            Towers = _level.GetComponentsInChildren<Tower>().ToList();
+            TowersCount = Towers.Count;
         }
 
         void ReceiveTowerData()
         {
             for (int i = 0; i < TowersCount; i++)
             {
-                _towerDatas.Add(_towers[i].Data);
+                TowerDatas.Add(Towers[i].Data);
             }
         }
 
-        public static void LinkingTowers(List<TowerData> towers) //ters de gelebilir
-        {
-            for (var i = 0; i < TowersCount; i++)
-            {
-                towers[i].LinkedTowerIDs.Clear();
-
-                int next = towers[(i + 1) % TowersCount].UniqID; //sonra gelenin id'sini alıyor, bu artan da olabilir azalan da
-                towers[i].LinkedTowerIDs.Add(next);
-                
-               // print("index: " + (i + 1) % TowersCount + " id: " + next);
-            }
-        }
+        // public static void LinkingTowers(List<TowerData> towers) //ters de gelebilir
+        // {
+        //     for (var i = 0; i < TowersCount; i++)
+        //     {
+        //         towers[i].LinkedTowerIDs.Clear();
+        //
+        //         int next = towers[(i + 1) % TowersCount].UniqID; //sonra gelenin id'sini alıyor, bu artan da olabilir azalan da
+        //         towers[i].LinkedTowerIDs.Add(next);
+        //         
+        //        // print("index: " + (i + 1) % TowersCount + " id: " + next);
+        //     }
+        // }
 
         public void SettingNeighbours()
         {
             for (var i = 0; i < TowersCount; i++)
             {
-                _towerDatas[i].NeighbourIDs.Clear();
+                TowerDatas[i].NeighbourIDs.Clear();
                 
                 int previousID = i - 1;
                 if (previousID < 0)
                     previousID = TowersCount - 1;
-                int previous =  _towerDatas[previousID].UniqID;
+                int previous =  TowerDatas[previousID].UniqID;
                 
-                int next = _towerDatas[(i + 1) % TowersCount].UniqID;
+                int next = TowerDatas[(i + 1) % TowersCount].UniqID;
                 
-                _towerDatas[i].NeighbourIDs.Add(previous);
-                _towerDatas[i].NeighbourIDs.Add(next);
+                TowerDatas[i].NeighbourIDs.Add(previous);
+                TowerDatas[i].NeighbourIDs.Add(next);
             }
         }
 
         public static void RestoreBullets()
         {
-            _towers.ForEach(t => t.RestoreBullets());
+            Towers.ForEach(t => t.RestoreBullets());
         }
 
         public static void ResetTowerSelectionColors()
         {
-            _towerDatas.ForEach(t=>t.ColorHandler.ToOriginalColor());
+            TowerDatas.ForEach(t=>t.ColorHandler.ToOriginalColor());
         }
 
         public static void EnableClickability()
         {
-            _towerDatas.ForEach(t=>t.EnableSelection());
+            TowerDatas.ForEach(t=>t.EnableSelection());
         }
 
         public static void DisableClickability()
         {
-            _towerDatas.ForEach(t=>t.DisableSelection());
+            TowerDatas.ForEach(t=>t.DisableSelection());
         }
         
 
@@ -142,7 +141,7 @@ namespace Towers
         private void OnDrawGizmos()
         {
             Gizmos.color = Color.yellow;
-            foreach (var tower in _towers)
+            foreach (var tower in Towers)
             {
                 foreach (var linkedTowerID in tower.Data.LinkedTowerIDs)
                 {
